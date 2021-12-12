d2e1c2328f144204b3ef264c4c497fc784951d0d
Esat Kemal Ekren <kemalekren@hotmail.com>
2021-12-12 20:13:27 +0300

    Initial Commit

---
+++ DemoFirestore/DemoFirestoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoFirestoreApp.swift
+//  DemoFirestore
+//
+//  Created by Kemal Ekren on 12.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct DemoFirestoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
