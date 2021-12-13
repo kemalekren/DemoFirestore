c4c10e57b065b8144d9da3b6a7b54ebb79f49a87
Esat Kemal Ekren <kemalekren@hotmail.com>
2021-12-13 21:58:05 +0300

    Firebase Login Screen Complete

--- DemoFirestore/ContentView.swift
+++ DemoFirestore/ContentView.swift
@@ -6,14 +6,363 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Home()
     }
 }
 
+struct Home: View {
+    @State var show = false
+    @State var status = UserDefaults.standard.value(forKey: "status") as? Bool ?? false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                if status {
+                    Homescreen()
+                } else {
+                    ZStack {
+                        NavigationLink(isActive: self.$show) {
+                            Register(show: self.$show)
+                        } label: {
+                            Text("")
+                        }
+                        .hidden()
+                        Login(show: self.$show)
+                    }
+                }
+            }
+            .navigationTitle("")
+            .navigationBarHidden(true)
+            .navigationBarBackButtonHidden(true)
+            .onAppear {
+                
+                NotificationCenter.default.addObserver(forName: NSNotification.Name("status"), object: nil, queue: .main) { (_) in
+                    self.status = UserDefaults.standard.value(forKey: "status") as? Bool ?? false
+                }
+            }
+        }
+    }
+}
+
+struct Login: View {
+    @State var color = Color.black.opacity(0.7)
+    @State var email = ""
+    @State var password = ""
+    @State var visible = false
+    @Binding var show : Bool
+    @State var alert = false
+    @State var error = ""
+    
+    var body: some View {
+        ZStack{
+            ZStack(alignment: .topTrailing) {
+                GeometryReader { _ in
+                    VStack {
+                        Image("logo")
+                            .resizable()
+                        //                .frame(width: 100, height: 100)
+                        
+                        Text("Log into Your account")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(self.color)
+                            .padding(.top, 35)
+                        
+                        TextField("Email", text: self.$email)
+                            .padding()
+                            .background(RoundedRectangle(cornerRadius: 4).stroke(self.email != "" ? Color("Color") : color, lineWidth: 2))
+                            .padding(.top, 25)
+                        
+                        HStack(spacing: 15) {
+                            VStack {
+                                if self.visible {
+                                    TextField("Password", text: self.$password)
+                                } else {
+                                    SecureField("Password", text: self.$password)
+                                }
+                            }
+                            
+                            Button {
+                                self.visible.toggle()
+                            } label: {
+                                Image(systemName: self.visible ? "eye.slash.fill" : "eye.fill")
+                                    .foregroundColor(self.color)
+                            }
+                        }
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 4).stroke(self.password != "" ? Color("Color") : color, lineWidth: 2))
+                        .padding(.top, 25)
+                        
+                        HStack {
+                            Spacer()
+                            Button {
+                                
+                            } label: {
+                                Text("Forget Password")
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color("Color"))
+                            }
+                            
+                        }
+                        .padding(.top, 20)
+                        
+                        Button {
+                            self.verify()
+                            
+                        } label: {
+                            Text("Log In")
+                                .foregroundColor(.white)
+                                .padding(.vertical)
+                                .frame(width: UIScreen.main.bounds.width - 50)
+                        }
+                        .background(Color("Color"))
+                        .cornerRadius(10)
+                        .padding(.top, 25)
+                        
+                        
+                    }
+                    .padding(.horizontal, 25)
+                    
+                }
+                Button {
+                    self.show.toggle()
+                } label: {
+                    Text("Register")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("Color"))
+                }
+                .padding()
+            }
+            
+            if self.alert {
+                ErrorView(alert: self.$alert, error: self.$error)
+            }
+        }
+    }
+    
+    func verify() {
+        if self.email != "" && self.password != "" {
+            Auth.auth().signIn(withEmail: self.email, password: self.password) { result, err in
+                if let error = err {
+                    self.error = error.localizedDescription
+                    self.alert.toggle()
+                    return
+                }
+                UserDefaults.standard.set(true, forKey: "status")
+                NotificationCenter.default.post(name: NSNotification.Name("status"), object: nil)
+            }
+        } else {
+            self.error = "Please fill the empty areas"
+            self.alert.toggle()
+        }
+    }
+}
+
+struct Register: View {
+    @State var color = Color.black.opacity(0.7)
+    @State var email = ""
+    @State var password = ""
+    @State var rePassword = ""
+    @State var visible = false
+    @State var revisible = false
+    @Binding var show : Bool
+    @State var alert = false
+    @State var errorMessage = ""
+    
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            GeometryReader { _ in
+                VStack {
+                    Image("logo")
+                        .resizable()
+                    
+                    Text("Log into Your account")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(self.color)
+                        .padding(.top, 35)
+                    
+                    TextField("Email", text: self.$email)
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 4).stroke(self.email != "" ? Color("Color") : color, lineWidth: 2))
+                        .padding(.top, 25)
+                    
+                    HStack(spacing: 15) {
+                        VStack {
+                            if self.visible {
+                                TextField("Password", text: self.$password)
+                            } else {
+                                SecureField("Password", text: self.$password)
+                            }
+                        }
+                        
+                        Button {
+                            self.visible.toggle()
+                        } label: {
+                            Image(systemName: self.visible ? "eye.slash.fill" : "eye.fill")
+                                .foregroundColor(self.color)
+                        }
+                    }
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 4).stroke(self.password != "" ? Color("Color") : color, lineWidth: 2))
+                    .padding(.top, 25)
+                    
+                    HStack(spacing: 15) {
+                        VStack {
+                            if self.visible {
+                                TextField("Re-Enter", text: self.$rePassword)
+                            } else {
+                                SecureField("Re-Enter", text: self.$rePassword)
+                            }
+                        }
+                        
+                        Button {
+                            self.visible.toggle()
+                        } label: {
+                            Image(systemName: self.revisible ? "eye.slash.fill" : "eye.fill")
+                                .foregroundColor(self.color)
+                        }
+                    }
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 4).stroke(self.rePassword != "" ? Color("Color") : color, lineWidth: 2))
+                    .padding(.top, 25)
+                    
+                    Button {
+                        if !self.email.isEmpty, self.password == rePassword {
+                            Auth.auth().createUser(withEmail: self.email, password: self.password) { result, err in
+                                if let error = err {
+                                    self.errorMessage = error.localizedDescription
+                                    self.alert.toggle()
+                                    return
+                                }
+                                
+                                UserDefaults.standard.set(true, forKey: "status")
+                                NotificationCenter.default.post(name: NSNotification.Name("status"), object: nil)
+                            }
+                        } else if self.email.isEmpty {
+                            self.errorMessage = "Email Can't be Empty"
+                            self.alert.toggle()
+                        } else {
+                            self.errorMessage = "Passwords not matched"
+                            self.alert.toggle()
+                        }
+                        
+                    } label: {
+                        Text("Register")
+                            .foregroundColor(.white)
+                            .padding(.vertical)
+                            .frame(width: UIScreen.main.bounds.width - 50)
+                    }
+                    .background(Color("Color"))
+                    .cornerRadius(10)
+                    .padding(.top, 25)
+                    
+                    
+                }
+                .padding(.horizontal, 25)
+                
+            }
+            Button {
+                self.show.toggle()
+            } label: {
+                Image(systemName: "chevron.left")
+                    .foregroundColor(Color("Color"))
+            }
+            .padding()
+        }
+        .navigationTitle("")
+        .navigationBarHidden(true)
+        .navigationBarBackButtonHidden(true)
+    }
+}
+
+struct Homescreen : View {
+    
+    var body: some View{
+        
+        VStack{
+            
+            Text("Logged successfully")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.black.opacity(0.7))
+            
+            Button(action: {
+                
+                try! Auth.auth().signOut()
+                UserDefaults.standard.set(false, forKey: "status")
+                NotificationCenter.default.post(name: NSNotification.Name("status"), object: nil)
+                
+            }) {
+                
+                Text("Log out")
+                    .foregroundColor(.white)
+                    .padding(.vertical)
+                    .frame(width: UIScreen.main.bounds.width - 50)
+            }
+            .background(Color("Color"))
+            .cornerRadius(10)
+            .padding(.top, 25)
+        }
+    }
+}
+
+struct ErrorView : View {
+    
+    @State var color = Color.black.opacity(0.7)
+    @Binding var alert : Bool
+    @Binding var error : String
+    
+    var body: some View{
+        
+        GeometryReader{_ in
+            
+            VStack{
+                
+                HStack{
+                    
+                    Text(self.error == "RESET" ? "Message" : "Error")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(self.color)
+                    
+                    Spacer()
+                }
+                .padding(.horizontal, 25)
+                
+                Text(self.error == "RESET" ? "Password reset link has been sent successfully" : self.error)
+                .foregroundColor(self.color)
+                .padding(.top)
+                .padding(.horizontal, 25)
+                
+                Button(action: {
+                    
+                    self.alert.toggle()
+                    
+                }) {
+                    
+                    Text(self.error == "RESET" ? "Ok" : "Cancel")
+                        .foregroundColor(.white)
+                        .padding(.vertical)
+                        .frame(width: UIScreen.main.bounds.width - 120)
+                }
+                .background(Color("Color"))
+                .cornerRadius(10)
+                .padding(.top, 25)
+                
+            }
+            .padding(.vertical, 25)
+//            .frame(width: UIScreen.main.bounds.width)
+            .background(Color.white)
+            .cornerRadius(15)
+        }
+        .background(Color.black.opacity(0.35).edgesIgnoringSafeArea(.all))
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
--- DemoFirestore/DemoFirestoreApp.swift
+++ DemoFirestore/DemoFirestoreApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct DemoFirestoreApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
